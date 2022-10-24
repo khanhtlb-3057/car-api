e3ad6a78f7b4ee42647d316bb5a290f9ec327a66
tran.le.bao.khanh <SUN-ASTERISK\tran.le.bao.khanh@b123057-pc.sun-asterisk.com>
2022-10-24 16:57:11 +0700

    API get users

---
+++ src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ){}
+
+  async findAll(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    return await this.usersRepository.findOneBy({ id: id });
+  }
+}
